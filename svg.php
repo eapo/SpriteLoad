d3709d7a33af2c6b63bf8a933825705a0cfd4590
eapo <e@eaposztrof.com>
2023-02-06 00:16:09 +0100

    Update svg.php

--- svg.php
+++ svg.php
@@ -3,10 +3,10 @@
   $query = htmlspecialchars($_GET['q']);
   $file = "solid.svg";
 
-  function svg($q){
+  function svg($q,$f){
     $default = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128z"></path></svg>';
     $query = (!empty($q))?$q:'file-o';
-    $xml = simplexml_load_file($file) or die($default);
+    $xml = simplexml_load_file($f) or die($default);
     foreach($xml->symbol as $symbol) {
       if ($symbol['id'] == $query) {
         $viewBox = $symbol['viewBox'];
@@ -20,5 +20,5 @@
       return $default;
     }
   }
-  echo svg($query);
+  echo svg($query,$file);
 ?>
